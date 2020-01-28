9781b9ae790122891359956ef7df536328d44865
cjslack <40322960+cjslack@users.noreply.github.com>
2020-01-28 18:13:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static('public'));
 app.post('/', async (req, res) => {
     const {token, region, story} = req.body;
     try {
-        let logger = require('logzio-nodejs').createLogger({token, host: `listener${region ? '-' + region: ''}.logz.io`});
+        let logger = require('logzio-nodejs').createLogger({token, host: `listener${region ? '-' + region : ''}.logz.io`});
         let logs = await writeStory(story);
         logs.forEach(line => {
-            logger.log(line);
+            logger.log(line)
         });
         res.status(200).send('Succesfully shipped story');
     } catch (e) {
